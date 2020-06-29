6454eb1a4709e22a5603b17a7f9c08856defc7d1
Denis Nazarov <d@denisnazarov.com>
2020-06-29 00:29:13 -0400

    add meta tags

--- pages/[id].js
+++ pages/[id].js
@@ -5,12 +5,22 @@ import styles from "./[id].module.css";
 
 import { getAllPostIds, getPostData } from "../lib/posts";
 
+const twitterHandle = "@Iiterature";
+const siteName = "denis nazarov";
+
 export default function Post({ postData: { title, id, date, contentHtml } }) {
+  const pageTitle = `${title} — Denis Nazarov`;
   return (
     <Layout>
       <Head>
-        <title>{title}</title>
+        <title>{pageTitle}</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
+        <meta name="twitter:card" content="summary" key="twcard" />
+        <meta name="twitter:creator" content={twitterHandle} key="twhandle" />
+
+        <meta property="og:site_name" content={siteName} key="ogsitename" />
+        <meta property="og:title" content={pageTitle} key="ogtitle" />
       </Head>
       <div className={styles.postBody}>
         <h1>{title}</h1>
